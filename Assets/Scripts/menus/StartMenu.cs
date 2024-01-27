06229bb6feecc2ff34338c1c7a80b058fc66ac72
Michael Vandendriessche <michael.vandendriessche1991@gmail.com>
2024-01-27 15:28:58 +0100

    updated menus

--- Assets/Scripts/menus/StartMenu.cs
+++ Assets/Scripts/menus/StartMenu.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class StartMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        Time.timeScale = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartGame()
     {
-        
+
+        Time.timeScale = 1;
+        gameObject.SetActive(false);
     }
 }
