543b38e7037868a17eda1f140cfb922d7a468435
Michael Vandendriessche <michael.vandendriessche1991@gmail.com>
2024-01-28 14:17:03 +0100

    Game over sounds

--- Assets/Scripts/menus/GameOverMenu.cs
+++ Assets/Scripts/menus/GameOverMenu.cs
@@ -6,6 +6,26 @@ using UnityEngine.SceneManagement;
 public class GameOverMenu : MonoBehaviour
 {
 
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip sirens;
+    [SerializeField] private AudioClip crashSound;
+
+    private void Start()
+    {
+        audioSource.PlayOneShot(crashSound);
+        Invoke("PlaySirenSounds", crashSound.length -1); // The -1 is a hack because I dont want to edit the audio file right now
+        
+    }
+
+    private void PlaySirenSounds()
+    {
+        audioSource.PlayOneShot(sirens);
+    }
+
+    private void OnDestroy()
+    {
+        audioSource.Stop();
+    }
     public void ExitGame()
     {
         Application.Quit();
